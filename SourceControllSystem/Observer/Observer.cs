31d01dc439a83f33f925280dd11ffa1b20d8b885
אינהורן חיה < @mbjcomp.local>
2024-03-17 18:05:10 +0200

    first

---
+++ SourceControllSystem/Observer/Observer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SourceControllSystem.Observer
+{
+    internal class Observer
+    {
+    }
+}
--- SourceControllSystem/Program.cs
+++ SourceControllSystem/Program.cs
@@ -1,6 +1,7 @@
 ﻿
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
 Console.WriteLine("RACHELI IS A GOOD FREIND!!!!!!");
-
-
+Console.WriteLine("RACHELI IS A GOOD FREIND!!!!!!");
+Console.WriteLine("RACHELI IS A GOOD FREIND!!!!!!");
