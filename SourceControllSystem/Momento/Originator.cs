889f63e9b1b8e3b39c58025cc2c932adde3472bc
וינר רחל1 < 1@C9>
2024-04-01 17:54:43 +0300

    momento #1

---
+++ SourceControllSystem/Momento/Originator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SourceControllSystem.Momento
+{
+    public class Originator
+    {
+
+        private string _state;
+
+        public Originator(string state)
+        {
+            this._state = state;
+            Console.WriteLine("Originator: My initial state is: " + state);
+        }
+
+        public void DoSomething()
+        {
+            Console.WriteLine("Originator: I'm doing something important.");
+            this._state = this.GenerateRandomString(30);
+            Console.WriteLine($"Originator: and my state has changed to: {_state}");
+        }
+
+        private string GenerateRandomString(int length = 10)
+        {
+            string allowedSymbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string result = string.Empty;
+
+            while (length > 0)
+            {
+                result += allowedSymbols[new Random().Next(0, allowedSymbols.Length)];
+
+                Thread.Sleep(12);
+
+                length--;
+            }
+
+            return result;
+        }
+    }
+}
